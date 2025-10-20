e11edc047fc6155ac9a25ea4774ad1a2d83a2640
Up <8566666@gmail.com>
2025-10-20 11:41:49 +0800

    Update golang repository URL and branch

--- LEDE/diy-part-patches.sh
+++ LEDE/diy-part-patches.sh
@@ -118,7 +118,7 @@ cp -f $GITHUB_WORKSPACE/patch/v2dat/Makefile feeds/packages/utils/v2dat/Makefile
 
 # golang1.25
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang -b 1.25 feeds/packages/lang/golang
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
